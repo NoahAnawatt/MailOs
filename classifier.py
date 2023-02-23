8a46d65be194254884869a3abac656b2c7fee604
Eric Christensen <noahanawatt@icloud.com>
2023-02-22 22:39:58 -0600

    Added dataset, 64 scans

--- classifier.py
+++ classifier.py
@@ -3,7 +3,9 @@ from tensorflow.keras import layers
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 
-epochs = 10
+epochs = 220
+batch_size = 5
+train_dir = './data_scans/'
 
 # Define the input shape of the images
 input_shape = (128, 128, 3)
@@ -29,7 +31,6 @@ model.compile(
 )
 
 # Set up the data generator
-batch_size = 2
 train_datagen = ImageDataGenerator(
     rescale=1./255,
     shear_range=0.2,
@@ -37,21 +38,31 @@ train_datagen = ImageDataGenerator(
     horizontal_flip=True
 )
 train_generator = train_datagen.flow_from_directory(
-    "sample_data/",
+    train_dir,
     target_size=(128, 128),
     batch_size=batch_size,
     class_mode="binary",
-    classes=["junk", "real"]
+    classes=["business", "junk"]
 )
 
 # Train the model
+print(str(train_generator.samples)+' images')
 steps_per_epoch = train_generator.samples // batch_size
-model.fit(
+history = model.fit(
     train_generator,
     epochs=epochs,
     steps_per_epoch=steps_per_epoch
 )
+history = history.history['loss']
+print(history)
 
 # Save the model
 model.save("model.h5")
 
+import matplotlib.pyplot as plt
+plt.plot(history)
+plt.ylabel('Validation Loss')
+plt.xlabel('Epochs')
+plt.show
+
+plt.show()
--- interpeter.py
+++ interpeter.py
@@ -8,10 +8,10 @@ model = tf.keras.models.load_model("model.h5")
 
 # Set up the image processing parameters
 img_width, img_height = 128, 128
-classes = ["junk", "real"]
+classes = ["business","junk"]
 
 # Process each image in the training directory
-train_dir = "./sample_data/"
+train_dir = "./data_scans/"
 if not os.path.exists(train_dir):
     raise ValueError("Training directory does not exist!")
 for class_name in classes:
@@ -25,5 +25,5 @@ for class_name in classes:
         pred = model.predict(images)
         class_idx = int(np.round(pred[0]))
         class_name = classes[class_idx]
-        print(f"Predicted class for image {img_path}: {class_name}, image is actually {img_path.split('/')[2]}")
+        print(f"Predicted class for image {img_path}: {class_name}")
 
